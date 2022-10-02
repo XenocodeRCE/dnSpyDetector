359a5aaf89148c3a3edac423c1bd2f3bd1dc3412
Dainen Dunn <lild31415@gmail.com>
2022-10-02 00:05:58 -0500

    Added System.Diagnostics.Debugger dnSpy hook detection

--- dnSpyDetector/dnSpyDetector/Program.cs
+++ dnSpyDetector/dnSpyDetector/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+
 namespace dnSpyDetector
 {
     class Program
@@ -10,8 +12,9 @@ namespace dnSpyDetector
         [System.Runtime.InteropServices.DllImport("kernel32.dll")]
         public static extern IntPtr GetProcAddress(IntPtr hModule, string procedureName);
 
-        static void Main(string[] args) {
-
+        static void Main(string[] args)
+        {
+            int hookCount = 0;
             Console.WriteLine("Checking the presence of dnSpy hooks ...");
 
             IntPtr kernel32 = LoadLibrary("kernel32.dll");
@@ -21,10 +24,10 @@ namespace dnSpyDetector
             System.Runtime.InteropServices.Marshal.Copy(GetProcessId, data, 0, 1);
 
             //32-bit relative jump = opcode 0xE9
-            if (data[0] == 0xE9) {
+            if (data[0] == 0xE9)
+            {
                 Console.WriteLine($"IsDebuggerPresent hook detected ...");
-                Console.ReadKey();
-                return;
+                hookCount++;
             }
 
             GetProcessId = GetProcAddress(kernel32, "CheckRemoteDebuggerPresent");
@@ -32,13 +35,32 @@ namespace dnSpyDetector
             System.Runtime.InteropServices.Marshal.Copy(GetProcessId, data, 0, 1);
 
             //32-bit relative jump = opcode 0xE9
-            if (data[0] == 0xE9) {
+            if (data[0] == 0xE9)
+            {
                 Console.WriteLine($"CheckRemoteDebuggerPresent hook detected ...");
-                Console.ReadKey();
-                return;
+                hookCount++;
             }
 
 
+            var debuggerType = typeof(Debugger);
+            System.Reflection.MethodInfo[] methods = debuggerType.GetMethods();
+            var getMethod = debuggerType.GetMethod("get_IsAttached");
+
+            IntPtr targetAddre = getMethod.MethodHandle.GetFunctionPointer();
+            data = new byte[1];
+            System.Runtime.InteropServices.Marshal.Copy(targetAddre, data, 0, 1);
+
+            if (data[0] == 0x33)
+            {
+                Console.WriteLine($"System.Diagnostics.Debugger hook detected ...");
+                hookCount++;
+            }
+
+            if (hookCount == 0)
+            {
+                Console.WriteLine("No dnSpy hooks found!");
+            }
+            
             Console.ReadKey();
         }
     }
